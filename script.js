06fe75b98db0c5563fdd507b7387c0040f1c4a98
IcyMilkyWay09 <jcratty09@gmail.com>
2025-05-23 22:15:17 -0400

    Initial Upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+const slider = document.getElementById("myRange");
+const contentElements = document.querySelectorAll(".content");
+
+slider.addEventListener("input", function() 
+{
+  const sliderValue = parseInt(this.value);
+
+  contentElements.forEach(element => {
+    element.style.display = "none";
+  });
+
+  document.getElementById(`content-${sliderValue}`).style.display = "block";
+});
+
+*/
